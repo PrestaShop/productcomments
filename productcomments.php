4e32c9c3ef434e2c8308b8f24308d245e210d7d5
GoT <PierreRambaud@users.noreply.github.com>
2020-11-09 09:55:52 +0100

    Update productcomments.php
    Co-authored-by: Progi1984 <progi1984@gmail.com>

--- productcomments.php
+++ productcomments.php
@@ -241,7 +241,7 @@ class ProductComments extends Module implements WidgetInterface
             $comment->validate();
         } elseif ($id_product_comment = (int) Tools::getValue('noabuseComment')) {
             ProductComment::deleteReports($id_product_comment);
-            Tools::redirectAdmin(Context::getContext()->link->getAdminLink('AdminModules', true, [], ['configure' => $this->name]));
+            Tools::redirectAdmin($this->context->link->getAdminLink('AdminModules', true, [], ['configure' => $this->name]));
         }
 
         $this->_clearcache('productcomments_reviews.tpl');
