907df5cdd7cf91124939078ce42dd7215bd9aca0
leemyongpakvn <leemyongpakvn@gmail.com>
2022-06-26 07:40:09 +0700

    Update install.sql
    Co-authored-by: Krystian Podemski <kpodemski@users.noreply.github.com>

--- install.sql
+++ install.sql
@@ -63,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `PREFIX_product_comment_report` (
   `id_product_comment` int(10) unsigned NOT NULL,
   `id_customer` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id_product_comment`, `id_customer`)
-) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8mb4;
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
 
 INSERT IGNORE INTO `PREFIX_product_comment_criterion` VALUES ('1', '1', '1');
 
