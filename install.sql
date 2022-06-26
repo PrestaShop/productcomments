be055594c00a4c348e416b8dcb68e4406881dc94
leemyongpakvn <leemyongpakvn@gmail.com>
2022-06-26 07:36:35 +0700

    Update install.sql
    Co-authored-by: Krystian Podemski <kpodemski@users.noreply.github.com>

--- install.sql
+++ install.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `PREFIX_product_comment_criterion_product` (
   `id_product_comment_criterion` int(10) unsigned NOT NULL,
   PRIMARY KEY(`id_product`, `id_product_comment_criterion`),
   KEY `id_product_comment_criterion` (`id_product_comment_criterion`)
-) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8mb4;
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `PREFIX_product_comment_criterion_lang` (
   `id_product_comment_criterion` INT(11) UNSIGNED NOT NULL ,
