a28314bc20caeafb7af84e25d3f91ba878486609
Lorenz Meyer <lm@bible.ch>
2024-02-29 09:30:37 +0100

    Use on delete cascade on foreign keys

--- src/Entity/ProductCommentGrade.php
+++ src/Entity/ProductCommentGrade.php
@@ -37,7 +37,7 @@ class ProductCommentGrade
     /**
      * @ORM\Id
      * @ORM\ManyToOne(targetEntity="ProductComment")
-     * @ORM\JoinColumn(name="id_product_comment", referencedColumnName="id_product_comment")
+     * @ORM\JoinColumn(name="id_product_comment", referencedColumnName="id_product_comment", onDelete="CASCADE")
      */
     private $comment;
 
