902e2fac40beb7a85900981178c9be05abc0e759
Daniel Hlaváček <daniel.hlavacek@hotmail.cz>
2025-10-29 17:46:25 +0100

    Update productcomments.php

--- productcomments.php
+++ productcomments.php
@@ -455,11 +455,7 @@ class ProductComments extends Module implements WidgetInterface
             $helper->table = $this->name;
             $helper->table_id = 'waiting-approval-productcomments-list';
             $helper->token = Tools::getAdminTokenLite('AdminModules');
-            $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false, [], [
-			    'configure'   => $this->name,
-			    'tab_module'  => $this->tab,
-			    'module_name' => $this->name,
-			]);
+            $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false, [], ['configure'   => $this->name, 'tab_module'  => $this->tab, 'module_name' => $this->name]);
             $helper->no_link = true;
 
             $return .= $helper->generateList($comments, $fields_list);
@@ -484,11 +480,7 @@ class ProductComments extends Module implements WidgetInterface
         $helper->table = $this->name;
         $helper->table_id = 'reported-productcomments-list';
         $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false, [], [
-		    'configure'   => $this->name,
-		    'tab_module'  => $this->tab,
-		    'module_name' => $this->name,
-		]);
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false, [], ['configure'   => $this->name, 'tab_module'  => $this->tab, 'module_name' => $this->name]);
         $helper->no_link = true;
 
         $return .= $helper->generateList($comments, $fields_list);
@@ -572,11 +564,7 @@ class ProductComments extends Module implements WidgetInterface
         $helper->title = $this->trans('Review Criteria', [], 'Modules.Productcomments.Admin');
         $helper->table = $this->name . 'criterion';
         $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false, [], [
-		    'configure'   => $this->name,
-		    'tab_module'  => $this->tab,
-		    'module_name' => $this->name,
-		]);
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false, [], ['configure'   => $this->name, 'tab_module'  => $this->tab, 'module_name' => $this->name]);
 
         return $helper->generateList($criterions, $fields_list);
     }
@@ -597,11 +585,7 @@ class ProductComments extends Module implements WidgetInterface
         $helper->table = $this->name;
         $helper->table_id = 'approved-productcomments-list';
         $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false, [], [
-		    'configure'   => $this->name,
-		    'tab_module'  => $this->tab,
-		    'module_name' => $this->name,
-		]);
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false, [], ['configure'   => $this->name, 'tab_module'  => $this->tab, 'module_name' => $this->name]);
         $helper->no_link = true;
 
         $page = ($page = Tools::getValue('submitFilter' . $helper->list_id)) ? (int) $page : 1;
